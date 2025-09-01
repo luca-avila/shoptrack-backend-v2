f0a42eaefecd4563f0f6503cccd6788d276aa14e
luca-avila <avilaluca61@gmail.com>
2025-09-01 15:52:08 -0300

    Clean up files to get a empty structure to work with

--- shoptrack/models/__init__.py
+++ shoptrack/models/__init__.py
@@ -1,8 +1,1 @@
-# Import all models
-from .base import BaseModel
-from .user import User
-from .product import Product
-from .history import History
-from .session import Session
-
-__all__ = ['BaseModel', 'User', 'Product', 'History', 'Session']
+# Import all models - import your model classes here
