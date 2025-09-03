803e54cbc5675a1100bfa8526507a06faf9baa7e
luca-avila <avilaluca61@gmail.com>
2025-09-03 14:39:01 -0300

    Define User model

--- shoptrack/models/user.py
+++ shoptrack/models/user.py
@@ -1,1 +1,20 @@
+from .base import BaseModel
+from typing import Optional, List
+from sqlalchemy import String, Boolean
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
+
+class User(BaseModel):
+    __tablename__ = 'users'
+    
+    username: Mapped[str] = mapped_column(String(80), unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(String(255), nullable=False)
+    email: Mapped[Optional[str]] = mapped_column(String(120), unique=True)
+
+    products: Mapped[List['Product']] = relationship(
+        back_populates='owner',
+        cascade='all, delete-orphan'
+        )
+
+    def __repr__(self):
+        return f"<User(id={self.id}, username='{self.username}')>"
