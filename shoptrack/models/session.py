225373efbaaff02c855ff132fb907687dbba6922
luca-avila <avilaluca61@gmail.com>
2025-09-03 15:10:07 -0300

    Define session table

--- shoptrack/models/session.py
+++ shoptrack/models/session.py
@@ -1,1 +1,13 @@
+from .base import BaseModel
+from sqlalchemy import String, ForeignKey, DateTime
+from datetime import datetime
+from sqlalchemy.orm import Mapped, mapped_column
 
+class Session(BaseModel):
+    __tablename__ = "session"
+
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    expires: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False)
+
+    def __repr__(self):
+        return f"<Session(id={self.id}, session_id={self.id}, user_id={self.user_id}, expires={self.expires})>"
