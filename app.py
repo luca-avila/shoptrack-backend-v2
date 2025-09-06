da1910da96bda665a3439bc057f58f45d6fb2277
luca-avila <avilaluca61@gmail.com>
2025-09-06 12:28:04 -0300

    Implement app.py file in root folder importing create_app function

---
+++ app.py
@@ -0,0 +1,10 @@
+from shoptrack import create_app
+import os
+
+app = create_app()
+
+if __name__ == '__main__':
+    
+    port = os.getenv('PORT', 5000)
+
+    app.run(host='0.0.0.0', port=port, debug=True)
